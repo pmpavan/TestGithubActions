628a57b03e93b44a22e06687ebd1003d657de62a
Pavan P M <pmpavan@outlook.com>
2024-06-02 13:42:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.pavanpm.testgithubactions"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pavanpm.testgithubactions"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
